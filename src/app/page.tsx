5a95bdfa550d15680a8e82d5c7c0fcb82d62604b
Lohan Santos <lohansantosborges@gmail.com>
2025-09-22 16:32:46 +0000

    bote essa img https://i.imgur.com/MWLZoRN.jpeg (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,7 +111,7 @@ export default function Page() {
         />
         <div className="absolute -bottom-14 left-1/2 -translate-x-1/2">
           <Image
-            src="https://instagram.fbel10-1.fna.fbcdn.net/v/t51.2885-19/525781313_18069955340118184_3013822007023453116_n.jpg?stp=dst-jpg_s150x150_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.fbel10-1.fna.fbcdn.net&_nc_cat=104&_nc_oc=Q6cZ2QEo_wGtXJJvmSlZOaOGDJsjR__iqRr21DH8XkYNFwGa2j1Ynn_GD1SR5vs966PW0KQ&_nc_ohc=7jvGF4nvCMsQ7kNvwElDsmc&_nc_gid=BhQLg5blS1hu_htMfDrhyA&edm=ALGbJPMBAAAA&ccb=7-5&oh=00_AfbNQJZ3P41cdBeWq7Lnzb4HNlyUvA8nX1SWlmIrM6OPog&oe=68CF8FB1&_nc_sid=7d3ac5"
+            src="https://i.imgur.com/MWLZoRN.jpeg"
             alt="Foto de Perfil"
             width={120}
             height={120}
