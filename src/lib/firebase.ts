ebb6605fdcbe12d1e0c72b7c46dd452cb72a7839
Lohan Santos <lohansantosborges@gmail.com>
2025-09-22 18:37:03 +0000

    coloque isso no .env:   "projectId": "studio-7900944804-69342",   "appId

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-7900944804-69342",
-  "appId": "1:29138376636:web:dec14f2b402d3dc1103db9",
-  "apiKey": "AIzaSyCGg2t9A6d7LO7oxZXLQye9qzqF4HZNWTc",
-  "authDomain": "studio-7900944804-69342.firebaseapp.com",
-  "storageBucket": "studio-7900944804-69342.appspot.com",
-  "messagingSenderId": "29138376636"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
 export { db };
